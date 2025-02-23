4035a738e4019d32ea4334be79bcf6a29811c910
Adarsh Gupta <gadarsh318@gmail.com>
2025-02-23 09:16:23 +0545

    add JWT token verification middleware

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+interface CustomRequest extends Request {
+    userId?: string;
+}
+
+export default function verifyToken(req: CustomRequest, res: Response, next: NextFunction): void {
+    const authHeader = req.headers["authorization"] as string;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        res.status(403).json({ message: "Unauthorized 1" });
+        return;
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+        res.status(403).json({ message: "No token provided" });
+        return;
+    }
+
+    jwt.verify(token, JWT_SECRET as string, (err, decoded: any) => {
+        if (err) {
+            return res.status(401).json({ message: "Unauthorized 2" });
+        }
+        req.userId = decoded.userId;
+        next();
+    });
+}
